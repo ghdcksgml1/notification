8105f4b84947636d15789a1a6249bb91bb983d0f
ghdcksgml1 <ghdcksgml2@naver.com>
2023-06-26 18:20:06 +0900

    feat: Cors 설정

---
+++ src/main/kotlin/com/wintersoft/notification/config/cors/CorsGlobalConfiguration.kt
@@ -0,0 +1,19 @@
+package com.wintersoft.notification.config.cors
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@Configuration
+@EnableWebFlux
+class CorsGlobalConfiguration : WebFluxConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
+}
